d77026c55fda847e5cccca8b2e4563c8fe22f17b
core-cookbooks <nagalakshmi.n@cloudenablers.com>
2015-04-20 18:20:35 +0530

    Initial Release

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "chef_handler"
+maintainer       "Cloudenablers."
+maintainer_email "nagalakshmi.n@cloudenablers.com"
+license          "Apache 2.0"
+description      "Distribute and enable Chef Exception and Report handlers"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.1.4"
